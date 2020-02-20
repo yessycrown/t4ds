5566c2a3b46cab8e9a63ed707a93d6b489a08e1c
Jordan Schupbach <jordans1882@gmail.com>
2020-02-20 18:39:20 +0000

    Deploy update of the site

--- js/content-pagination.js
+++ js/content-pagination.js
@@ -1,7 +1,7 @@
 (function () {
   function pagination(){
     $("div.pagination").jPages({
-      containerID : "archivelist",
+      containerID : "workshoplist",
       previous: "Preivous",
       next: "Next",
       perPage: 8
@@ -12,17 +12,17 @@
       var keyword = $('input#post-search').val();
       if (keyword.length==0) {
         $.when(
-          $('ul#archivelist li').removeClass('jp-hidden').show(400)
+          $('ul#workshoplist li').removeClass('jp-hidden').show(400)
         ).done(function() {
           pagination();
         });
       }else{
         $.when(
-          $('ul#archivelist li').each(function(index, el) {
+          $('ul#workshoplist li').each(function(index, el) {
             var postTitle = $(el).find('a.post-link').text().toLowerCase();
             if (postTitle.indexOf(keyword.toLowerCase())==-1){
               $(el).hide(400);
-            } 
+            }
             else $(el).removeClass('jp-hidden').show(400)
           })
           ).done(function() {
