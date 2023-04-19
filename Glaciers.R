74b1546382922eafebe016167b0733428dc06020
Ben Holmgren <44175897+benholmgren@users.noreply.github.com>
2023-04-19 08:23:19 -0600

    Add a group discussion time

--- Glaciers.R
+++ Glaciers.R
@@ -23,6 +23,8 @@ point.in.polygon(1,2,coords[,1],coords[,2])
 point.in.polygon(269000,5424500,coords[,1],coords[,2])
                  
 
+which.
+
 first_five <- head(glaciers_66, 5)
 first_five
 plot(first)
@@ -70,6 +72,7 @@ plot(Diag[["diagram"]], barcode = TRUE)
 ## grid diag
 n <- 300
 XX <- circleUnif(n)
+XX
 
 ## Ranges of the grid
 Xlim <- c(-1.8, 1.8)
@@ -82,3 +85,24 @@ h <- .3  #bandwidth for the function kde
 #Distance Function Diagram of the sublevel sets
 Diag1 <- gridDiag(XX, distFct, lim = lim, by = by, sublevel = TRUE,
                   printProgress = TRUE) 
+Diag2 <- gridDiag(XX, kde, lim = lim, by = by, sublevel = FALSE,
+                  library = "Dionysus", location = TRUE, printProgress = TRUE, h = h)
+#plot
+par(mfrow = c(2, 2))
+plot(XX, cex = 0.5, pch = 19)
+title(main = "Data")
+plot(Diag1[["diagram"]])
+title(main = "Distance Function Diagram")
+plot(Diag2[["diagram"]])
+title(main = "Density Persistence Diagram")
+one <- which(Diag2[["diagram"]][, 1] == 1)
+plot(XX, col = 2, main = "Representative loop of grid points")
+for (i in seq(along = one)) {
+  points(Diag2[["birthLocation"]][one[i], , drop = FALSE], pch = 15, cex = 3,
+         col = i)
+  points(Diag2[["deathLocation"]][one[i], , drop = FALSE], pch = 17, cex = 3,
+         col = i)
+  for (j in seq_len(dim(Diag2[["cycleLocation"]][[one[i]]])[1])) {
+    lines(Diag2[["cycleLocation"]][[one[i]]][j, , ], pch = 19, cex = 1, col = i)
+  }
+}
